df44b9b66a6be55891d2c73144b65a1dca30592d
hydiexe <nexuszeoviennazabrizkie@gmail.com>
2023-11-23 07:09:12 +0700

    soft deletes

--- app/Program.php
+++ app/Program.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Program extends Model
 {
+    use SoftDeletes;
     public function edulevel() {
         return $this->belongsTo('App\Edulevel');
     }
